2425446886633a8e267fec3efaf010ebce102134
Santiago <santiago.zapata2588@gmail.com>
2023-11-22 12:04:24 +0000

    Added text

--- Home.py
+++ Home.py
@@ -22,5 +22,5 @@ from PIL import Image
 image = Image.open('Resources/logo_Pungo.png')
 st.image(image)
 
-st.write("# DIGITALIZATION WITH POSITIVE IMPACT")
+st.write("# DIGITALIZATION WITH POSITIVE IMPACTS)
 st.write("### Welcome to AUCA: la herramienta digital para la gestión de la energía y las emisiones de carbono en la industria")
