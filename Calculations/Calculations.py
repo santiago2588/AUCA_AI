82b5fb49c360d18d39a6e14443f3673de3cb3719
Santiago <santiago.zapata2588@gmail.com>
2023-11-23 16:59:09 +0000

    Added text

--- Calculations/Calculations.py
+++ Calculations/Calculations.py
@@ -102,13 +102,12 @@ prod_diaria=df_prod_mod.groupby('Fecha')['Produccion'].sum()
 energy_int_total=results.groupby('Fecha')['Contenido energia MJ'].sum()
 
 #Resultados por proceso
-co2_total_process=results.groupby('ID proceso')['Emisiones kg CO2-eq'].sum()
-costo_total_process=results.groupby('ID proceso')['Costo energia USD'].sum()
-energy_total_process=results.groupby('ID proceso')['Contenido energia MJ'].sum()
+#co2_total_process=results.groupby('ID proceso')['Emisiones kg CO2-eq'].sum()
+#costo_total_process=results.groupby('ID proceso')['Costo energia USD'].sum()
+#energy_total_process=results.groupby('ID proceso')['Contenido energia MJ'].sum()
 
-results_process=pd.concat([co2_total_process,costo_total_process,energy_total_process],axis=1)
-results_process=pd.DataFrame(results_process)
-process_hotspot_co2=results_process.loc[results_process['Emisiones kg CO2-eq'].idxmax(),'ID proceso']
+#results_process=pd.concat([co2_total_process,costo_total_process,energy_total_process],axis=1)
+process_hotspot_co2=results.loc[results['Emisiones kg CO2-eq'].idxmax(),'ID proceso']
 #process_hotspot_costo=results_process.loc[results_process['Costo energia USD'].idxmax(),'ID proceso']
 #process_hotspot_energy=results_process.loc[results_process['Contenido energia MJ'].idxmax(),'ID proceso']
 
