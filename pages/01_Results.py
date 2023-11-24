5a8296a7a25301d80825dc3de9abc4b78733a6df
Santiago <santiago.zapata2588@gmail.com>
2023-11-24 12:05:46 +0000

    Added text

--- pages/01_Results.py
+++ pages/01_Results.py
@@ -170,10 +170,12 @@ with st.expander('Personalized Advice',expanded=True):
         llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
         st.info(llm(input_text))
 
+    prompt=f'The {process_hotspot_co2} is currently the highest carbon emitter. How can we reduce its emissions?'
+
     if not openai_api_key.startswith('sk-'):
         st.warning('Please enter your OpenAI API key!', icon='⚠')
-    if submitted and openai_api_key.startswith('sk-'):
-        generate_response(text)
+    if openai_api_key.startswith('sk-'):
+        generate_response(prompt)
 
 
     st.success(""" 
