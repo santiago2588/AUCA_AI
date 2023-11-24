e26eef40b67febacd443d042a6612f3d9233bc3f
Santiago <santiago.zapata2588@gmail.com>
2023-11-24 16:51:27 +0000

    Added text

--- pages/02_Copilot.py
+++ pages/02_Copilot.py
@@ -5,7 +5,9 @@ openai_api_key = st.text_input('OpenAI API Key')
 
 def generate_response(input_text):
     llm = OpenAI(temperature=0.3, openai_api_key=openai_api_key)
-    st.info(llm(input_text))
+    response=llm(input_text)
+    st.write(response)
+    return response
 
 prompt_opportunities=f'I want you to act as an energy and carbon emissions consultant with wide experience in the industry. The process {process_hotspot_co2}, which include the equipment {equipment_hotspot_co2}, is currently the highest carbon emitter in my plant, due to the use of the fuel {fuel_hotspot_co2}. I want you to identify the most relevant and easy-to-implement improvement opportunities to reduce the CO2 emissions of this process.'
 prompt_action_plan=f'I want you to act as an energy and carbon emissions consultant with wide experience in the industry. The process {process_hotspot_co2}, which include the equipment {equipment_hotspot_co2}, is currently the highest carbon emitter in my plant, due to the use of the fuel {fuel_hotspot_co2}. I want you to generate a professional and detailed climate strategy, including Reduction Targets and Indicators.'
@@ -15,12 +17,13 @@ if not openai_api_key.startswith('sk-'):
 if openai_api_key.startswith('sk-'):
     tab1, tab2=st.tabs(['Improvement opportunities','Action plan'])
     with tab1:
-        generate_response(prompt_opportunities)
+        response=generate_response(prompt_opportunities)
     with tab2:
-        generate_response(prompt_action_plan)
+        response=generate_response(prompt_action_plan)
 
 # Assume 'response' is the response object from the OpenAI API
-if generate_response['choices'][0]['finish_reason'] == 'length':
+
+if response['choices'][0]['finish_reason'] == 'length':
     st.write("The response is incomplete.")
 else:
     st.write("The response is complete.")
