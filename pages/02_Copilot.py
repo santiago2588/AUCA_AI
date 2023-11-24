fe1e152ea0567ce792fb4d58e7a9fd6dddeb82ce
Santiago <santiago.zapata2588@gmail.com>
2023-11-24 16:37:29 +0000

    Added text

--- pages/02_Copilot.py
+++ pages/02_Copilot.py
@@ -7,8 +7,8 @@ def generate_response(input_text):
     llm = OpenAI(temperature=0.3, openai_api_key=openai_api_key)
     st.info(llm(input_text))
 
-prompt_opportunities=f'I want you to act as an energy and carbon emissions consultant with wide experience in the industry. The {process_hotspot_co2}, {equipment_hotspot_co2},{fuel_hotspot_co2} are currently the highest carbon emitters in my plant. Help me to identify the most relevant and easy-to-implement improvement opportunities to reduce the CO2 emissions'
-prompt_action_plan=f'I want you to act as an energy and carbon emissions consultant with wide experience in the industry. The {process_hotspot_co2}, {equipment_hotspot_co2},{fuel_hotspot_co2} are currently the highest carbon emitters in my plant. Help me to generate a professional and detailed climate strategy, including Reduction Targets and Indicators'
+prompt_opportunities=f'I want you to act as an energy and carbon emissions consultant with wide experience in the industry. The process {process_hotspot_co2}, the equipment {equipment_hotspot_co2}, and the fuel {fuel_hotspot_co2} are currently the highest carbon emitters in my plant. Help me to identify the most relevant and easy-to-implement improvement opportunities to reduce the CO2 emissions'
+prompt_action_plan=f'I want you to act as an energy and carbon emissions consultant with wide experience in the industry. The process {process_hotspot_co2}, the equipment {equipment_hotspot_co2}, and the fuel {fuel_hotspot_co2} are currently the highest carbon emitters in my plant. Help me to generate a professional and detailed climate strategy, including Reduction Targets and Indicators'
 
 if not openai_api_key.startswith('sk-'):
     st.warning('Please enter your OpenAI API key!', icon='⚠')
